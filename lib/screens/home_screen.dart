c7f4090baa2782e84b572451d37eac3b8e4d96f2
johnny <johnnynguyenwork@gmail.com>
2020-08-15 22:27:19 +1000

    added markers

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -46,7 +46,181 @@ class _HomeScreenState extends State<HomeScreen> {
               snippet: "No Fire",
             )),
       );
+      _addMarkerList();
     });
+  }
+
+  void _addMarkerList() {
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("1"),
+          position: LatLng(-30.501446, 145.702932),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Gunderbrooka",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("2"),
+          position: LatLng(-32.705166, 145.567595),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Yathong",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("3"),
+          position: LatLng(-33.072026, 145.865550),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Nohimbinnie Nature Reserve",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("4"),
+          position: LatLng(-29.095658, 141.426478),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Stuart National Park",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("5"),
+          position: LatLng(-30.962721, 142.693419),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Mutawinji",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("7"),
+          position: LatLng(-33.768513, 146.281073),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Jimberoo National Park",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("8"),
+          position: LatLng(-34.035388, 146.194336),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Cococapra",
+            snippet: "Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("9"),
+          position: LatLng(-31.769398, 150.101581),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Coolah Tops",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("10"),
+          position: LatLng(-31.621455, 152.016470),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Bugan National Reserve",
+            snippet: "No Fire",
+          )),
+    );
+    _markers.add(
+      Marker(
+          //todo change marker id -> unique
+          markerId: MarkerId("0"),
+          position: LatLng(-33.777837, 147.800537),
+          onTap: () {
+            showCupertinoModalBottomSheet(
+                context: context,
+                builder: (context, scrollController) {
+                  return Container();
+                });
+          },
+          infoWindow: InfoWindow(
+            title: "Pullabooka",
+            snippet: "No Fire",
+          )),
+    );
   }
 
   @override
